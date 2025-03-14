4a7d817d9115f7a501cce8d242d49e0cf62844fd
study8practice <156129782+study8practice@users.noreply.github.com>
2025-03-14 17:28:53 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,64 @@
+import { hoverChangeExperience } from "./hoverChangeExperience.js";
+
+hoverChangeExperience(
+  ".innovasfera",
+  `Trabalhei na InnovaSfera, onde tive a oportunidade de participar
+                de vários projetos para diversos clientes. Nessa trajetória,
+                trabalhei em várias criações de sites, sistemas e interfaces web
+                e mobile com a equipe de tecnologia da empresa. A InnovaSfera é
+                referência em qualidade e inovação em suas soluções
+                tecnológicas, seja em desenvolvimento ou infraestrutura.`,
+  "UX Designer & Desenvolvedor Frontend",
+  "InnovaSfera",
+  "Maio 2024 - No momento (1 ano)"
+);
+
+hoverChangeExperience(
+  ".codelab",
+  `Na CodeLab Studio, lidero a criação e implementação de soluções tecnológicas voltadas para o design de produto e desenvolvimento de sistemas web. Nosso foco é entregar produtos inovadores, intuitivos e de alta performance para empresas de diversos segmentos.`,
+  "UX Designer & Desenvolvedor Frontend",
+  "CodeLab Studio",
+  "Jan 2023 - No momento (2 anos)"
+);
+
+hoverChangeExperience(
+  ".digitalhouse",
+  `Trabalhei como instrutor conteudista em frontend na
+  Digital House, o trabalho consistia em criar conteúdos para as
+  aulas de especialização frontend. Alguns temas dos conteúdos
+  realizados foram: React, Redux, TypeScript, Testes,
+  GraphQL, Next.js, MUI, React Hook Form e styled-components.`,
+  "Professor conteudista em Frontend",
+  "Digital House",
+  "Nov 2021 - Nov 2022 (1 ano)"
+);
+
+hoverChangeExperience(
+  ".zuplae",
+  `Trabalhei como Social Media e Designer na Zuplae que é um escola
+  de programação, onde teve como foco as criações de conteúdos sobre
+  programação para a comunidade dev através das redes sociais.`,
+  "Social Media e UX Designer",
+  "Zuplae",
+  "Jan 2022 - Abril 2022 (4 meses)"
+);
+
+hoverChangeExperience(
+  ".codigofontetv",
+  `Trabalhei como Social Media e Designer no Código Fonte TV, 
+  onde teve como foco as criações de conteúdos sobre programação 
+  para a comunidade dev através das redes sociais.`,
+  "Social Media e UX Designer",
+  "Código Fonte TV",
+  "Jun 2021 - Jan 2022 (8 meses)"
+);
+
+hoverChangeExperience(
+  ".contweb",
+  `Realizei o estágio na ContWeb, uma empresa de contabilidade. 
+  Onde pela primeira vez tive experiência com o React. Além do
+  desenvolvimento frontend criei os designs da nova plataforma da empresa.`,
+  "Developer frontend e UX Designer",
+  "ContWeb",
+  "Set 2021 - Nov 2021 (3 meses)"
+);
